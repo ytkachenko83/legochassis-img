3e131acf58bc560c311a93f7cfcf86c79e639794
Yuri Tkachenko <yura.tkachenko@gmail.com>
2024-05-14 14:11:53 -0700

    assignment 8

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'b03203944ad88d1750b2053dde44846b675f3a02'
+AESD_ASSIGNMENTS_VERSION = '8fd5ae1f9f9e248312f3b6a18809f3598f545ce5'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
---
+++ base_external/package/aesdchar/aesdchar.mk
@@ -0,0 +1,19 @@
+##############################################################
+#
+# AESDCHAR
+#
+##############################################################
+AESDCHAR_VERSION = '8fd5ae1f9f9e248312f3b6a18809f3598f545ce5'
+AESDCHAR_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-ytkachenko83.git'
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
+
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
+
+define AESDCHAR_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin/
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
