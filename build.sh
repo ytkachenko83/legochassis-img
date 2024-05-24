9c6260e5b5d47b58d0b14998ce47b607ff6ed004
Yuri Tkachenko <yura.tkachenko@gmail.com>
2024-05-24 00:46:57 -0700

    buildroot pkg

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #Script to build buildroot configuration
-#Author: Siddhant Jajoo
 
-source shared.sh
+source shared.sh ${1:-y}
 
 EXTERNAL_REL_BUILDROOT=../base_external
 git submodule init
@@ -16,18 +15,17 @@ if [ ! -e buildroot/.config ]
 then
 	echo "MISSING BUILDROOT CONFIGURATION FILE"
 
-	if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
+	if [ -e ${LEGOCHASSIS_MODIFIED_DEFCONFIG} ]
 	then
-		echo "USING ${AESD_MODIFIED_DEFCONFIG}"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
+		echo "USING ${LEGOCHASSIS_MODIFIED_DEFCONFIG}"
+		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${LEGOCHASSIS_MODIFIED_DEFCONFIG_REL_BUILDROOT}
 	else
-		echo "Run ./save_config.sh to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
+		echo "Run ./save_config.sh to save this as the default configuration in ${LEGOCHASSIS_MODIFIED_DEFCONFIG}"
 		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG}
+		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${LEGOCHASSIS_DEFAULT_DEFCONFIG}
 	fi
 else
 	echo "USING EXISTING BUILDROOT CONFIG"
 	echo "To force update, delete .config or make changes using make menuconfig and build again."
 	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
-
 fi
--- shared.sh
+++ shared.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 # Shared definitions for buildroot scripts
-
+USE_QEMU="${1:-y}"
 # The defconfig from the buildroot directory we use for qemu builds
 QEMU_DEFCONFIG=configs/qemu_aarch64_virt_defconfig
+RPI_DEFCONFIG=configs/raspberrypi4_64_defconfig
 # The place we store customizations to the qemu configuration
-MODIFIED_QEMU_DEFCONFIG=base_external/configs/aesd_qemu_defconfig
+MODIFIED_QEMU_DEFCONFIG=base_external/configs/legochassis_qemu_defconfig
+MODIFIED_DEFCONFIG=base_external/configs/legochassis_defconfig
 # The defconfig from the buildroot directory we use for the project
-AESD_DEFAULT_DEFCONFIG=${QEMU_DEFCONFIG}
-AESD_MODIFIED_DEFCONFIG=${MODIFIED_QEMU_DEFCONFIG}
-AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT=../${AESD_MODIFIED_DEFCONFIG}
+if [ "$USE_QEMU" = "y" ]
+then
+    LEGOCHASSIS_DEFAULT_DEFCONFIG=${QEMU_DEFCONFIG}
+    LEGOCHASSIS_MODIFIED_DEFCONFIG=${MODIFIED_QEMU_DEFCONFIG}
+else
+    LEGOCHASSIS_DEFAULT_DEFCONFIG=${RPI_DEFCONFIG}
+    LEGOCHASSIS_MODIFIED_DEFCONFIG=${MODIFIED_DEFCONFIG}
+fi    
+LEGOCHASSIS_MODIFIED_DEFCONFIG_REL_BUILDROOT=../${LEGOCHASSIS_MODIFIED_DEFCONFIG}
